22d7bc8f6e9c74318e353c5324b42976d4503e00
Nikita <vysokij96@bk.ru>
2021-11-30 00:25:31 +0300

    addedlocalstorage

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,47 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img src="./assets/img.png" alt="btc" class="btc">
+    <HelloWorld/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld";
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+  #app{
+    box-sizing: border-box;
+    margin: 0;
+  }
+.btc{
+  position: absolute;
+  width: 60px;
+  left: 50%;
+  top: 30px;
+  animation: rotation 10s infinite;
 }
+@media (max-width: 600px) {
+  .btc{
+    width: 30px;
+  }
+}
+  @keyframes rotation {
+    from{
+      transform: rotateX(0deg);
+    }
+    to{
+      transform: rotateX(360deg);
+
+    }
+  }
 </style>
